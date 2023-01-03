d742d097b96d0737e915c80f77ef0dc479e43130
Nguyễn Hồng Sơn <sonnh2@vmodev.com>
2023-01-04 02:16:17 +0700

    add data

--- data.py
+++ data.py
@@ -1,21 +1,70 @@
 BENH = {
-    'Tiểu đường': {
-        'nen': ['Rau xanh', 'Thanh đạm'],
-        'konen': ['Tinh bột', 'Mỡ động vật'],
+    'Bệnh tiểu đường': {
+        'nen': ['rau xanh', 'Thanh đạm'],
+        'konen': ['thực phẩm chứa nhiều tinh bột', 'mỡ động vật', 'đồ ngọt', 'các chất kích thích'],
     },
-    'Cao huyết áp': {
-        'nen': ['Rau xanh', 'Cà rốt', 'Nước'],
-        'konen': []
+    'Bệnh cao huyết áp': {
+        'nen': ['thực phẩm chứa nhiều chất xơ', 'rau xanh', 'các loại hạt hạ cholesterol xấu', 'mỡ thực vật'],
+        'konen': ['Muối', 'các chất kích thích']
+    },
+    'Bệnh mỡ máu': {
+        'nen': ['rau xanh', 'trái cây ít ngọt', 'thực phẩm giàu omega-3', 'hải sản'],
+        'konen': ['đồ ngọt', 'các chất kích thích']
+    },
+    'Bệnh Gout': {
+        'nen': ['rau xanh', 'Thanh đạm', 'trái cây ngọt', 'trái cây ít ngọt', 'mỡ thực vật', 'thực phẩm chứa nhiều vitamin c'],
+        'konen': ['các chất kích thích', 'nội tạng động vật', 'hải sản', 'thịt đỏ']
+    },
+    'Bệnh viêm khớp dạng thấp': {
+        'nen': ['thực phẩm giàu omega-3', 'rau xanh', 'các loại củ', 'các loại hạt'],
+        'konen': ['các chất kích thích', 'thịt đỏ', 'nội tạng động vật', 'gia vị cay']
     }
 }
 
 
 NHOM_THUC_PHAM = {
-    'Rau xanh': ['Rau má', 'Su hào']
+    'các loại hạt hạ cholesterol xấu': ['quả óc chó', 'hạnh nhân', 'đậu phộng', 'hạt dẻ cười', 'hạt điều'],
+    'thực phẩm chứa nhiều tinh bột': ['khoai tây', 'bánh mì', 'gạo', 'ngô'],
+    'thực phẩm chứa nhiều chất xơ': ['quả lê', 'dâu tây', 'bơ', 'táo', 'chuối', 'cà rốt', 'củ cải đường', 'khoai lang',
+                                     'bông cải xanh', 'hạnh nhân', 'ớt chuông', 'cải brussels'],
+    'rau xanh': ['bông cải xanh', 'khoai lang', 'su hào',  'cà rốt', 'cải thìa', 'cải xoong', 'rau chân vịt'],
+    'trái cây ngọt': ['mít', 'xoài', 'dứa', 'chuối', 'dưa hấu', 'lê'],
+    'trái cây ít ngọt': ['chanh leo', 'dâu tây', 'bưởi', 'bơ', 'cam', 'đu đủ', 'ổi', 'táo'],
+    'mỡ động vật': ['mỡ lợn', 'mỡ bò', 'mỡ cừu', 'mỡ gà', 'mỡ ngỗng', 'mỡ cá', 'dầu nhuyễn thể', 'dầu hào'],
+    'mỡ thực vật': ['dầu ngô', 'dầu hướng dương', 'dầu olive', 'dầu đậu tương'],
+    'gia vị cay': ['hành', 'ớt', 'tỏi', 'gừng', 'tiêu'],
+    'thực phẩm chứa nhiều vitamin e': ['hạt hạnh nhân', 'dầu olive', 'quả bơ', 'cá hồi', 'tôm', 'bông cải xanh'],
+    'thực phẩm chứa nhiều vitamin c': ['quả kiwi', 'dâu tây', 'súp lơ trắng', 'bông cải xanh', 'dưa lưới vàng', 'khoai tây', 'đu đủ'],
+    'thực phẩm có hàm lượng cholesterol cao': ['óc lợn', 'mỡ động vật', 'da gà', 'da vịt', 'da lợn', 'lòng đỏ trứng', 'chân giò', 'gân bò', 'nội tạng động vật'],
+    'đồ ngọt': ['chè', 'mứt', 'kẹo', 'bánh kem', 'kem', 'nước ngọt', 'nước tăng lực', 'nước trái cây đóng hộp', 'nước đường', 'mật ong'],
+    'các chất kích thích': ['rượu', 'bia', 'thuốc lá'],
+    'thịt đỏ': ['thịt chó', 'thịt bò', 'thịt trâu', 'thịt dê'],
+    'thực phẩm giàu omega-3': ['cá ngừ', 'cá hồi', 'cá thu', 'cá trích', 'hạt lanh', 'hạt chia', 'quả óc chó'],
+    'các loại củ': ['gừng', 'nghệ', 'tỏi'],
+    'các loại hạt': ['óc chó', 'hạnh nhân', 'đậu phộng', 'hạt chia', 'hạt lanh', 'hạt dẻ cười'],
+    'hải sản': ['tôm', 'tép', 'cua', 'ghẹ']
 }
 
 THUC_PHAM = {
-    'Rau má': {'calo': '1000'},
+    'da vịt': {'calo': '1000'}, 'nước đường': {'calo': '1000'}, 'cá hồi': {'calo': '1000'}, 'thịt trâu': {'calo': '1000'},
+    'mỡ động vật': {'calo': '1000'}, 'xoài': {'calo': '1000'}, 'gân bò': {'calo': '1000'}, 'lòng đỏ trứng': {'calo': '1000'},
+    'nước trái cây đóng hộp': {'calo': '1000'}, 'súp lơ trắng': {'calo': '1000'}, 'hạt chia': {'calo': '1000'}, 'hạt lanh': {'calo': '1000'},
+    'dầu olive': {'calo': '1000'}, 'dứa': {'calo': '1000'}, 'cải xoong': {'calo': '1000'}, 'bơ': {'calo': '1000'}, 'bưởi': {'calo': '1000'},
+    'su hào': {'calo': '1000'}, 'chè': {'calo': '1000'}, 'nghệ': {'calo': '1000'}, 'quả kiwi': {'calo': '1000'}, 'tỏi': {'calo': '1000'},
+    'đậu phộng': {'calo': '1000'}, 'cải brussels': {'calo': '1000'}, 'nước ngọt': {'calo': '1000'}, 'rượu': {'calo': '1000'}, 'thịt chó': {'calo': '1000'},
+    'bánh mì': {'calo': '1000'}, 'thịt bò': {'calo': '1000'}, 'ớt': {'calo': '1000'}, 'cải thìa': {'calo': '1000'}, 'nội tạng động vật': {'calo': '1000'},
+    'rau chân vịt': {'calo': '1000'}, 'dưa hấu': {'calo': '1000'}, 'bánh kem': {'calo': '1000'}, 'dầu đậu tương': {'calo': '1000'}, 'hạt dẻ cười': {'calo': '1000'},
+    'chanh leo': {'calo': '1000'}, 'mỡ ngỗng': {'calo': '1000'}, 'hành': {'calo': '1000'}, 'dầu nhuyễn thể': {'calo': '1000'}, 'đu đủ': {'calo': '1000'},
+    'mỡ cá': {'calo': '1000'}, 'quả bơ': {'calo': '1000'}, 'lê': {'calo': '1000'}, 'dưa lưới vàng': {'calo': '1000'}, 'da lợn': {'calo': '1000'}, 'mỡ gà': {'calo': '1000'}, 'táo': {'calo': '1000'},
+    'ớt chuông': {'calo': '1000'}, 'khoai tây': {'calo': '1000'}, 'củ cải đường': {'calo': '1000'}, 'cá ngừ': {'calo': '1000'}, 'cam': {'calo': '1000'},
+    'cá thu': {'calo': '1000'}, 'khoai lang': {'calo': '1000'}, 'cà rốt': {'calo': '1000'}, 'dầu hào': {'calo': '1000'}, 'thuốc lá': {'calo': '1000'},
+    'mứt': {'calo': '1000'}, 'kem': {'calo': '1000'}, 'dầu hướng dương': {'calo': '1000'}, 'gạo': {'calo': '1000'}, 'mỡ lợn': {'calo': '1000'},
+    'mít': {'calo': '1000'}, 'tiêu': {'calo': '1000'}, 'nước tăng lực': {'calo': '1000'}, 'mỡ bò': {'calo': '1000'}, 'dâu tây': {'calo': '1000'},
+    'ổi': {'calo': '1000'}, 'cá trích': {'calo': '1000'}, 'bia': {'calo': '1000'}, 'óc lợn': {'calo': '1000'}, 'kẹo': {'calo': '1000'},
+    'mật ong': {'calo': '1000'}, 'tôm': {'calo': '1000'}, 'quả lê': {'calo': '1000'}, 'hạt hạnh nhân': {'calo': '1000'}, 'bông cải xanh': {'calo': '1000'},
+    'dầu ngô': {'calo': '1000'}, 'ngô': {'calo': '1000'}, 'chuối': {'calo': '1000'}, 'hạnh nhân': {'calo': '1000'}, 'hạt điều': {'calo': '1000'},
+    'óc chó': {'calo': '1000'}, 'gừng': {'calo': '1000'}, 'quả óc chó': {'calo': '1000'}, 'chân giò': {'calo': '1000'}, 'thịt dê': {'calo': '1000'},
+    'mỡ cừu': {'calo': '1000'}, 'da gà': {'calo': '1000'}, 'tép': {'calo': '1000'}, 'cua': {'calo': '1000'}, 'ghẹ': {'calo': '1000'}
 }
 
 GIOITINH = ['Ông', 'Bà']
